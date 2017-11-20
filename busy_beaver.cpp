f6ea9b02775c6cae8777bec239e1b08bec7ccac1
carlosmartell97 <carlosmartell97@gmail.com>
2017-11-20 12:49:07 -0600

    card configuration for a 4-state machine printing 13 ones in 107 steps

--- busy_beaver.cpp
+++ busy_beaver.cpp
@@ -14,21 +14,58 @@ public:
 };
 
 int main(){
-  int numCards = 4;
-  vector<Card*> cards(numCards, new Card(0,0,0, 0,0,0));  // 4 cards, initially all zeroes
+  int numCards = 5;                                             // states
+  vector<Card*> cards(numCards, new Card(-1,-1,-1, -1,-1,-1));  // 4 cards + the final card (state), initially all -1
+
+  vector<short> tape(5000,0);           // pseudo infinite tape filled with zeroes
+  cards[1] = new Card(1,1,2, 1,0,2);
+  cards[2] = new Card(1,0,1, 0,0,3);
+  cards[3] = new Card(1,1,0, 1,0,4);
+  cards[4] = new Card(1,1,4, 0,1,1);
+
+  // print all cards
   for(int atCard=0; atCard<numCards; atCard++){
     printf("CARD %d\n", atCard);
     printf("0 %d%d%d\n", cards[atCard]->instructions[0][0],
-                      cards[atCard]->instructions[0][1],
-                      cards[atCard]->instructions[0][2]);
+    cards[atCard]->instructions[0][1],
+    cards[atCard]->instructions[0][2]);
     printf("1 %d%d%d", cards[atCard]->instructions[1][0],
-                      cards[atCard]->instructions[1][1],
-                      cards[atCard]->instructions[1][2]);
+    cards[atCard]->instructions[1][1],
+    cards[atCard]->instructions[1][2]);
     printf("\n\n");
   }
-  for(int card=0; card<numCards; card++){
-    delete cards[card];
+
+  int tapePosition = 2500;              // initial position is in the middle of the tape
+  int currentCard = 1;                  // initial card
+  short read;
+  int steps = 0;
+  while(true){
+    steps++;
+    read  = tape[tapePosition];
+    tape[tapePosition] = cards[currentCard]->instructions[read][0];
+    if(cards[currentCard]->instructions[read][1]==1){
+      tapePosition++;                   // move right
+    } else {
+      tapePosition--;                   // move left
+    }
+    currentCard = cards[currentCard]->instructions[read][2];
+    if(tapePosition<0 || tapePosition>=5000){
+      printf("\ntape not big enough, position=%d\n", tapePosition);
+    }
+    if(currentCard==0){
+      printf("\nMACHINE HALT\n");
+      int ones = 0;
+      for(int i=0; i<tape.size(); i++){
+        if(tape[i]==1) ones++;
+      }
+      printf("ones:%d  steps:%d\n", ones, steps);
+      break;
+    }
   }
+  // for(int card=0; card<numCards; card++){
+  //   delete cards[card];
+  // }
+
   printf("--DONE--\n");
   return 0;
 }
